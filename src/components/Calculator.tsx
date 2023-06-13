11fc7a2381286cd0121b10cb67958e265ded97ea
Liz Fosdick <lizfosdick@gmail.com>
2023-06-13 14:15:16 -0700

    Add currency component

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Currency from "./Currency";
 
 function Calculator() {
 
@@ -6,7 +7,7 @@ function Calculator() {
     <div className="mx-auto max-w-2xl rounded-3xl ring-1 ring-gray-200 dark:ring-gray-700 sm:mt-13 mt-16 lg:mx-0 lg:flex lg:max-w-none">
       <div className="px-4 py-6 md:p-8 sm:p-10 lg:flex-auto">
         <form method="POST" autoComplete="off" className="space-y-8 w-full">
-          
+          <Currency />
         </form>
       </div>
     </div>
---
+++ src/components/Currency.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { CURRENCIES } from "../constants";
+
+import { CurrencyProps } from "../types";
+
+function Currency({ activeCurrency, onCurrency }: CurrencyProps) {
+  return (
+    <div>
+      <label className="block text-sm font-medium text-gray-700 dark:text-white mb-1">
+        Currency
+        </label>
+        {Object.keys(CURRENCIES).map((currency) => {
+          <span key={currency} className="chip">{currency} </span>
+        })}
+    </div>
+  );
+}
+
+export default Currency;
