fc96755d247b1e630d14e2f4f5d6441988fcee0c
Liz Fosdick <lizfosdick@gmail.com>
2023-06-12 17:03:17 -0700

    Add form to calculator component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
-import ThemeSwitcher from "./components/ThemeSwitcher"
-import ThemeColorSwitcher from "./components/ThemeColorSwitcher"
+import ThemeSwitcher from "./components/ThemeSwitcher";
+import ThemeColorSwitcher from "./components/ThemeColorSwitcher";
+import Calculator from "./components/Calculator";
 
 function App() {
-
   return (
     <main className="bg-white dark:bg-black relative">
-      <div className="py-24 sm:py-32"></div>
-      <div className="mx-auto max-w-7x1 px-4 lg:px-0">
-        <ThemeSwitcher />
+      <div className="py-24 sm:py-32">
+        <div className="mx-auto max-w-7x1 px-4 lg:px-0">
+          <ThemeSwitcher />
 
-        <div className="mx-auto max-w-2x1 sm:text-center">
-          <ThemeColorSwitcher />
-          <h2 className="text-brand text-3xl font-bold tracking-tight sm:text-4x1 text-center">Profit Margin Calculator</h2>
-          <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-white text-center">Optimize your menu item prices to maximize profits by identifying the most effective selling prices.</p>
+          <div className="mx-auto max-w-2x1 sm:text-center">
+            <ThemeColorSwitcher />
+            <h2 className="text-brand text-3xl font-bold tracking-tight sm:text-4x1 text-center">
+              Profit Margin Calculator
+            </h2>
+            <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-white text-center">
+              Optimize your menu item prices to maximize profits by identifying
+              the most effective selling prices.
+            </p>
+          </div>
+
+          <Calculator />
         </div>
-
-        {/* TODO: Calculator */}
       </div>
     </main>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -3,8 +3,12 @@ import React, { useState, useEffect } from "react";
 function Calculator() {
 
   return (
-    <div>
-
+    <div className="mx-auto max-w-2xl rounded-3xl ring-1 ring-gray-200 dark:ring-gray-700 sm:mt-13 mt-16 lg:mx-0 lg:flex lg:max-w-none">
+      <div className="px-4 py-6 md:p-8 sm:p-10 lg:flex-auto">
+        <form method="POST" autoComplete="off" className="space-y-8 w-full">
+          
+        </form>
+      </div>
     </div>
   );
 }
